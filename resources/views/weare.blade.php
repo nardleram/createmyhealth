40264eedd38d4ebe8a8376b738e7ebb982305e65
nardleram <econosophy@gmail.com>
2023-01-31 10:17:08 +0000

    Refactor: director bio now component

--- resources/views/weare.blade.php
+++ resources/views/weare.blade.php
@@ -10,42 +10,77 @@
     <h1 class="font-serif text-5xl text-center mt-12 mb-6 text-lmhlPanel">We are... (website under construction)</h1>
 
     <div class="top-0 left-0 w-full h-full hidden profile-popup-bg z-40 bg-black opacity-95">
-        <div class="hidden mel mx-auto max-h-70% overflow-y-scroll top-32 pb-4 bg-lmhlMain1 opacity-100 text-lmhlBg1 font-sans text-sm p-4 md:p-10 w-11/12 md:w-4/5 z-50 rounded-md shadow-md">
-            <h5 class="font-serif text-right font-bold text-2xl -mt-4 -mr-1 md:-mt-8 md:-mr-4 text-lmhlPanel2 cursor-pointer" onclick="togglePopup('mel')">x</h5>
-            <h5 class="font-serif text-lg text-lmhlBg1 mb-2 px-6 font-bold">Mel Lagan</h5>
-            <p class="px-6 py-3 leading-loose">My nursing background is mainly in renal medicine caring for patients with chronic kidney disease. As a holistic therapist, I help people re-establish physical, emotional and mental balance and well-being, drawing on a number of modalities including classic reflexology, RLD (reflexology lymph drainage) and Bach Flower Essences.  I coach people to help them reframe stress and anxiety, recognise unhelpful thought patterns and behaviours and develop resilience in the face of life’s challenges. I am passionate about empowering my clients through education and knowledge sharing so they can begin to take charge of their own health and make lifestyle decisions that will support their well-being.</p>
-        </div>
-    
-        <div class="hidden sarah mx-auto max-h-70% overflow-y-scroll top-32 bg-lmhlMain1 opacity-100 text-lmhlBg1 font-sans text-sm p-4 md:p-10 w-11/12 md:w-4/5 z-50 rounded-md shadow-md">
-            <h5 class="font-serif text-right font-bold text-2xl -mt-4 -mr-1 md:-mt-8 md:-mr-4 text-lmhlPanel2 cursor-pointer" onclick="togglePopup('sarah')">x</h5>
-            <h5 class="font-serif text-lg text-lmhlBg1 mb-2 px-6 font-bold">Sarah Elder</h5>
-            <p class="px-6 py-3 leading-loose">Awaiting text</p>
-        </div>
-    
-        <div class="hidden elizabeth mx-auto max-h-70% overflow-y-scroll top-32 bg-lmhlMain1 opacity-100 text-lmhlBg1 font-sans text-sm p-4 md:p-10 w-11/12 md:w-4/5 z-50 rounded-md shadow-md">
-            <h5 class="font-serif text-right font-bold text-2xl -mt-4 -mr-1 md:-mt-8 md:-mr-4 text-lmhlPanel2 cursor-pointer" onclick="togglePopup('elizabeth')">x</h5>
-            <h5 class="font-serif text-lg text-lmhlBg1 mb-2 px-6 font-bold">Elizabeth Cooper</h5>
-            <p class="px-6 py-3 leading-loose">Awaiting text</p>
-        </div>
-    
-        <div class="hidden annette mx-auto max-h-70% overflow-y-scroll top-32 bg-lmhlMain1 opacity-100 text-lmhlBg1 font-sans text-sm p-4 md:p-10 w-11/12 md:w-4/5 z-50 rounded-md shadow-md">
-            <h5 class="font-serif text-right font-bold text-2xl -mt-4 -mr-1 md:-mt-8 md:-mr-4 text-lmhlPanel2 cursor-pointer" onclick="togglePopup('annette')">x</h5>
-            <h5 class="font-serif text-lg text-lmhlBg1 mb-2 px-6 font-bold">Annette Russell</h5>
-            <p class="px-6 py-3 leading-loose">I’m a biblioholic: happy when living between the covers of a book – preferably one that is hardbound and gorgeously illustrated! When confronted by a blank page, however, I’m equally happy to provide my own text and daydreams. Indeed, I’ve heard homeopathy defines health as the freedom to express creativity. Every blank page is an opportunity for creating health. In the nonfictional world, I teach, translate, proofread, write and edit, and have several years’ experience in the set-up and management of private medical practices in Germany.</p>
-        </div>
-    
-        <div class="hidden emily mx-auto max-h-70% overflow-y-scroll top-32 bg-lmhlMain1 opacity-100 text-lmhlBg1 font-sans text-sm p-4 md:p-10 w-11/12 md:w-4/5 z-50 rounded-md shadow-md">
-            <h5 class="font-serif text-right font-bold text-2xl -mt-4 -mr-1 md:-mt-8 md:-mr-4 text-lmhlPanel2 cursor-pointer" onclick="togglePopup('emily')">x</h5>
-            <h5 class="font-serif text-lg text-lmhlBg1 mb-2 px-6 font-bold">Emily Godfrey</h5>
-            <p class="px-6 py-3 leading-loose">Awaiting text</p>
-        </div>
-    
-        <div class="hidden anaïs mx-auto max-h-70% overflow-y-scroll top-32 bg-lmhlMain1 opacity-100 text-lmhlBg1 font-sans text-sm p-4 md:p-10 w-11/12 md:w-4/5 z-50 rounded-md shadow-md">
-            <h5 class="font-serif text-right font-bold text-2xl -mt-4 -mr-1 md:-mt-8 md:-mr-4 text-lmhlPanel2 cursor-pointer" onclick="togglePopup('anaïs')">x</h5>
-            <h5 class="font-serif text-lg text-lmhlBg1 mb-2 px-6 font-bold">Anaïs Russell</h5>
-            <p class="pt-6 px-6 leading-loose">My BA is in Philosophy, Psychology and History of Science from the University of Leeds. By day I have a full-time IT job, and pursue my other passions in my spare time: homeopathy at The School of Health, and  martial arts. I love long walks in our local woods and nature reserves, and hiking through the Dales or Peak District with my wolfdog.</p>
-            <p class="pt-3 pb-6 px-6 leading-loose">I am grateful to have this opportunity of supporting Create My Health CIC and the People’s Health Alliance with my IT skills and deep interest in all matters pertaining to health.</p>
-        </div>
+        <x-director-profile-bio class="elizabeth">
+            <x-slot:className>
+                elizabeth
+            </x-slot>
+
+            <x-slot:fullName>
+                Elizabeth Cooper
+            </x-slot>
+
+            <x-slot:bio>Awaiting text.</x-slot>
+        </x-director-profile-bio>
+
+        <x-director-profile-bio class="mel">
+            <x-slot:className>
+                mel
+            </x-slot>
+
+            <x-slot:fullName>
+                Mel Lagan
+            </x-slot>
+
+            <x-slot:bio>My nursing background is mainly in renal medicine caring for patients with chronic kidney disease. As a holistic therapist, I help people re-establish physical, emotional and mental balance and well-being, drawing on a number of modalities including classic reflexology, RLD (reflexology lymph drainage) and Bach Flower Essences.  I coach people to help them reframe stress and anxiety, recognise unhelpful thought patterns and behaviours and develop resilience in the face of life’s challenges. I am passionate about empowering my clients through education and knowledge sharing so they can begin to take charge of their own health and make lifestyle decisions that will support their well-being.</x-slot>
+        </x-director-profile-bio>
+
+        <x-director-profile-bio class="sarah">
+            <x-slot:className>
+                sarah
+            </x-slot>
+
+            <x-slot:fullName>
+                Sarah Elder
+            </x-slot>
+
+            <x-slot:bio>Awaiting text.</x-slot>
+        </x-director-profile-bio>
+
+        <x-director-profile-bio class="annette">
+            <x-slot:className>
+                annette
+            </x-slot>
+
+            <x-slot:fullName>
+                Annette Russell, MA
+            </x-slot>
+
+            <x-slot:bio>I’m a biblioholic: happy when living between the covers of a book – preferably one that is hardbound and gorgeously illustrated! When confronted by a blank page, however, I’m equally happy to provide my own text and daydreams. Indeed, I’ve heard homeopathy defines health as the freedom to express creativity. Every blank page is an opportunity for creating health. In the nonfictional world, I teach, translate, proofread, write and edit, and have several years’ experience in the set-up and management of private medical practices in Germany.</x-slot>
+        </x-director-profile-bio>
+
+        <x-director-profile-bio class="emily">
+            <x-slot:className>
+                emily
+            </x-slot>
+
+            <x-slot:fullName>
+                Emily Godfrey
+            </x-slot>
+
+            <x-slot:bio>Awaiting text.</x-slot>
+        </x-director-profile-bio>
+
+        <x-director-profile-bio class="anaïs">
+            <x-slot:className>
+                anaïs
+            </x-slot>
+
+            <x-slot:fullName>
+                Anaïs Russell
+            </x-slot>
+
+            <x-slot:bio>My BA is in Philosophy, Psychology and History of Science from the University of Leeds. By day I have a full-time IT job, and pursue my other passions in my spare time: homeopathy at The School of Health, and  martial arts. I love long walks in our local woods and nature reserves, and hiking through the Dales or Peak District with my wolfdog. <br>I am grateful to have this opportunity of supporting Create My Health CIC and the People’s Health Alliance with my IT skills and deep interest in all matters pertaining to health.</x-slot>
+        </x-director-profile-bio>
     </div>
 
     <div class="grid gap-3 lg:gap-4 md:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-3 md:px-5 sm:px-4">
@@ -58,10 +93,6 @@
             <x-slot:name>Elizabeth Cooper</x-slot>
 
             <x-slot:roles>Co-founder, Director</x-slot>
-
-            <x-slot:blurb class="bg-lmhlPanel3 elizabeth">
-                <p class="px-6 py-3">Awaiting text...</p>
-            </x-slot>
         </x-director-profile-panel>
 
         <x-director-profile-panel class="from-mel_profile_bg1 to-mel_profile_bg2">
@@ -71,11 +102,7 @@
 
             <x-slot:name>Mel Lagan</x-slot>
 
-            <x-slot:roles>Co-founder, Director, Nurse, Reflexology, HeartMath, Bach Flower Essences</x-slot>
-
-            <x-slot:blurb class="bg-lmhlPanel1">
-                <p class="px-6 py-3">My nursing background is mainly in renal medicine caring for patients with chronic kidney disease. As a holistic therapist, I help people re-establish physical, emotional and mental balance and well-being, drawing on a number of modalities including classic reflexology, RLD (reflexology lymph drainage) and Bach Flower Essences.  I coach people to help them reframe stress and anxiety, recognise unhelpful thought patterns and behaviours and develop resilience in the face of life’s challenges. I am passionate about empowering my clients through education and knowledge sharing so they can begin to take charge of their own health and make lifestyle decisions that will support their well-being.</p>
-            </x-slot>
+            <x-slot:roles>Co-founder, Director, Nurse, Reflexology, HeartMath, Bach Flower Essences, Earthing</x-slot>
         </x-director-profile-panel>
 
         <x-director-profile-panel class="from-sarah_profile_bg1 to-sarah_profile_bg2">
@@ -86,10 +113,6 @@
             <x-slot:name>Sarah Elder</x-slot>
 
             <x-slot:roles>Co-founder, Director</x-slot>
-
-            <x-slot:blurb class="bg-lmhlPanel2 sarah">
-                <p class="px-6 py-3">Awaiting text...</p>
-            </x-slot>
         </x-director-profile-panel>
 
         <x-director-profile-panel class="from-netty_profile_bg1 to-netty_profile_bg2">
@@ -100,10 +123,6 @@
             <x-slot:name>Annette Russell, MA</x-slot>
 
             <x-slot:roles>Co-founder, Director</x-slot>
-
-            <x-slot:blurb class="bg-lmhlPanel1 annette">
-                <p class="px-6 py-3">I’m a biblioholic: happy when living between the covers of a book – preferably one that is hardbound and gorgeously illustrated! When confronted by a blank page, however, I’m equally happy to provide my own text and daydreams. Indeed, I’ve heard homeopathy defines health as the freedom to express creativity. Every blank page is an opportunity for creating health. In the nonfictional world, I teach, translate, proofread, write and edit, and have several years’ experience in the set-up and management of private medical practices in Germany.</p>
-            </x-slot>
         </x-director-profile-panel>
 
         <x-director-profile-panel class="from-emily_profile_bg1 to-emily_profile_bg2">
@@ -114,10 +133,6 @@
             <x-slot:name>Emily Godfrey</x-slot>
 
             <x-slot:roles>Co-founder, Director</x-slot>
-
-            <x-slot:blurb class="bg-lmhlPanel2 emily">
-                <p class="px-6 py-3">Awaiting text...</p>
-            </x-slot>
         </x-director-profile-panel>
 
         <x-director-profile-panel class="from-anais_profile_bg1 to-anais_profile_bg2">
@@ -128,12 +143,6 @@
             <x-slot:name>Anaïs Russell</x-slot>
 
             <x-slot:roles>Data control, IT support</x-slot>
-
-            <x-slot:blurb class="bg-lmhlPanel3 anaïs">
-                <p class="px-6 pt-3">My BA is in Philosophy, Psychology and History of Science from the University of Leeds. By day I have a full-time IT job, and pursue my other passions in my spare time: homeopathy at The School of Health, and  martial arts. I love long walks in our local woods and nature reserves, and hiking through the Dales or Peak District with my wolfdog.</p>
-
-                <p class="px-6 pb-6 pt-3">I am grateful to have this opportunity of supporting Create My Health CIC and the People’s Health Alliance with my IT skills and deep interest in all matters pertaining to health.</p>
-            </x-slot>
         </x-director-profile-panel>
 
     </div>
