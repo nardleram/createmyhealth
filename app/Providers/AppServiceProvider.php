41728260d035fc072227867e4db5095b65f6b186
nardleram <econosophy@gmail.com>
2023-03-01 12:21:43 +0000

    Annoying problem migrating db in prod server, had to comment out line in AppServiceProvider, will reverse this soon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('photos', Photo::all());
+        // View::share('photos', Photo::all());
     }
 }
