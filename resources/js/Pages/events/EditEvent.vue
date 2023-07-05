07be3eb58096f49830ca04a4d487975a43fa6e0e
nardleram <econosophy@gmail.com>
2023-07-05 12:32:23 +0100

    CSS needed in both event add and event edit, just in case one starts with edit or with add

--- resources/js/Pages/events/EditEvent.vue
+++ resources/js/Pages/events/EditEvent.vue
@@ -204,3 +204,97 @@ const onSetLink = () => {
         .run()
 }
 </script>
+
+<style lang="scss">
+#editorDiv.editor__content > .ProseMirror a {
+    color: rgb(227 181 205);
+    cursor: pointer;
+}
+
+#editorDiv.editor__content > .ProseMirror a:hover {
+    color: rgb(213 144 180);
+}
+
+#editorDiv.editor__content {
+    font-size: 14px;
+    color: rgb(51 65 85);
+    text-align: left;
+
+    .ProseMirror a {
+        color: rgb(227 181 205);
+    }
+    .ProseMirror a:hover {
+        color: rgb(213 144 180);
+    }
+
+    .ProseMirror {
+        .tableWrapper {
+            tbody {
+                border-collapse: collapse;
+                table-layout: fixed;
+                width: 100%;
+                margin: 0;
+                overflow: hidden;
+
+                td, th {
+                min-width: 1em;
+                border: thin solid silver;
+                padding: 2px 3px;
+                vertical-align: top;
+                box-sizing: border-box;
+                position: relative;
+
+                    > * {
+                        margin-bottom: 0;
+                    }
+                }
+
+                th {
+                    font-weight: bold;
+                    text-align: left;
+                }
+            }
+
+            .selectedCell:after {
+                z-index: 2;
+                position: absolute;
+                content: "";
+                left: 0; right: 0; top: 0; bottom: 0;
+                background: rgba(200, 200, 255, 0.4);
+                pointer-events: none;
+            }
+
+            .column-resize-handle {
+                position: absolute;
+                right: -2px;
+                top: 0;
+                bottom: -2px;
+                width: 4px;
+                background-color: #adf;
+                pointer-events: none;
+            }
+
+            p {
+                margin: 0;
+            }
+        }
+    }
+}
+.selectedCell:after {
+    z-index: 2;
+    position: absolute;
+    content: "";
+    left: 0; right: 0; top: 0; bottom: 0;
+    pointer-events: none;
+}
+
+.resize-cursor {
+    cursor: ew-resize;
+    cursor: col-resize;
+}
+
+.tableWrapper {
+    padding: 1rem 0;
+    overflow-x: auto;
+}
+</style>
